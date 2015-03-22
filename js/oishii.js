c7767c8c21dd909d81523ff765b20b0711635d89
Jesse Jackson <jesse.publicmail@gmail.com>
2015-03-21 19:40:08 -0500

    Add info (getting started)

--- js/oishii.js
+++ js/oishii.js
@@ -2,7 +2,7 @@
 
 // Variables ----------
 
-
+var menu;
 
 
 // Functions ----------
@@ -13,6 +13,27 @@
 // EventListeners ----------
 
 
+
+
+// Google Maps -- https://developers.google.com/maps/documentation/javascript/examples/marker-simple
+function initialize() {
+	var myLatlng = new google.maps.LatLng(29.7331629,-95.4366254);
+	var mapOptions = {
+		center: myLatlng,
+		disableDefaultUI: false,
+		scrollwheel: false,
+		zoom: 15
+		};
+	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	var marker = new google.maps.Marker({
+		position: myLatlng,
+		map: map,
+		title: 'Oishii'
+		});
+	}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+// End Google Maps
 
 
 // Execute ----------
